f2e11749971e9c4ffe197751eec2c4a4ab2f6301
MyMac <mymac@MyMacs-MacBook-Pro.local>
2021-04-13 19:54:42 -0600

    commit' git remote add origin https://github.com/yugiman/w5d4.git git push -u origin master
    git remote add origin https://github.com/yugiman/w5d4.git
    git push -u origin master
    '

---
+++ app.js
@@ -0,0 +1,7 @@
+$('.navItem').each(function(){
+    $(this).css('color', 'white');
+})
+$('#nav').css('background-color', '#145');
+$('li').even().css('background-color', '#ccc');
+$('li').odd().css('background-color', '#aaa');
+$('input').val('Parker');
